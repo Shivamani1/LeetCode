940d282c4161a728efd6045d129b4a5e5805a0d8
Shivamani1 <111039019+Shivamani1@users.noreply.github.com>
2023-11-20 09:39:42 +0530

    Time: 3 ms (87.37%), Space: 42 MB (83.56%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -5,7 +5,7 @@ class Solution {
         ArrayList<Integer> a=new ArrayList<>();
         int i=0,j=0,k=0;
         
-        while(i<nums1.length&&j<nums2.length){
+        while(i!=nums1.length&&j!=nums2.length){
             if(nums1[i]==nums2[j]){
            a.add(nums1[i]);
                 i++;
