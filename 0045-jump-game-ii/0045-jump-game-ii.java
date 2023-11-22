78c0e797de489a98127b37b5bfd30b218d3734a2
Shivamani1 <111039019+Shivamani1@users.noreply.github.com>
2023-11-22 17:50:13 +0530

    Time: 1 ms (99.47%), Space: 44.5 MB (41.91%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,8 +1,8 @@
 class Solution {
     public int jump(int[] nums) {
       
-        int cur=0,flag=0,c=0;
-        for(int i=0;i<nums.length-1;i++){
+        int cur=0,flag=0,c=0,n=nums.length-1;
+        for(int i=0;i<n;i++){
             cur=Math.max(cur,i+nums[i]);
             if(i==flag){
              c++;
