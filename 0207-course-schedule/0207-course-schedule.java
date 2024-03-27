7b3b5a94c50f5d77c5fe6d86732a4436f05bf2af
Shivamani1 <111039019+Shivamani1@users.noreply.github.com>
2024-03-27 19:36:12 +0530

    Time: 5 ms (68.50%), Space: 44.2 MB (88.99%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,48 @@
+class Solution {
+    HashMap<Integer,List<Integer>> map=new HashMap();
+   boolean helper(int i,boolean[] vis,List<Integer> l){
+        if(vis[i])
+            return false;   
+        if(l==null||l.isEmpty())
+        return true;
+            vis[i]=true;
+
+            
+        for(int j=0;j<l.size();j++){
+    
+          // System.out.println(l.get(j)+""+map.get(l.get(j)));
+
+         if(!helper(l.get(j),vis,map.get(l.get(j))))
+             return false;   
+            if(!l.isEmpty())
+             l.remove(0);
+        }
+        
+
+        
+         
+        vis[i]=false;
+        return true;
+        
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+            boolean[] vis=new boolean[numCourses];
+        for(int i=0;i<prerequisites.length;i++){
+            if(!map.containsKey(prerequisites[i][0])){
+            map.put(prerequisites[i][0],new ArrayList());
+            map.get(prerequisites[i][0]).add(prerequisites[i][1]);
+             }
+           else
+           map.get(prerequisites[i][0]).add(prerequisites[i][1]);
+           }
+         //map.forEach((k, v) -> System.out.println(k +  "  " + v));
+            for(int i=0;i<numCourses;i++){
+            if(map.containsKey(i)){
+            if(!helper(i,vis,map.get(i)))
+                  return false;
+                
+            }
+        }
+      return true;  
+    }
+}
