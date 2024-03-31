80a88f8391f3cd6f41ba04157ba40cb9c05498af
Shivamani1 <111039019+Shivamani1@users.noreply.github.com>
2024-03-31 10:48:04 +0530

    Time: 54 ms (5.02%), Space: 46.1 MB (22.03%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,69 @@
+
+class Solution {
+   List<Integer> fl=new ArrayList();
+    HashMap<Integer,List<Integer>> map=new HashMap();
+    int a;
+   boolean helper(int i,boolean[] vis,List<Integer> l){
+        if(vis[i])
+            return false;   
+        if(l==null||l.isEmpty())
+        return true;
+            vis[i]=true;
+
+            
+        for(int j=0;j<l.size();j++){
+         a=l.get(j);
+         if(!helper(a,vis,map.get(a)))
+             return false;  
+            if(!fl.contains(l.get(0)))
+            fl.add(l.get(0));
+             l.remove(0);
+            j--;
+        }
+        
+
+        
+         
+        vis[i]=false;
+        return true;
+        
+    }
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+       
+            
+        
+            boolean[] vis=new boolean[numCourses];
+        for(int i=0;i<prerequisites.length;i++){
+            if(!map.containsKey(prerequisites[i][0])){
+            map.put(prerequisites[i][0],new ArrayList());
+            map.get(prerequisites[i][0]).add(prerequisites[i][1]);
+             }
+           else
+           map.get(prerequisites[i][0]).add(prerequisites[i][1]);
+           }
+         //map.forEach((k, v) -> System.out.println(k +  "  " + v));
+            for(int i=0;i<numCourses;i++){
+            if(map.containsKey(i)){
+            if(!helper(i,vis,map.get(i)))
+                  return new int[0];
+             if(!fl.contains(i))
+                fl.add(i);
+            }
+        }
+         int idx=0;
+     System.out.println(fl); 
+            int[] arr=new int[numCourses];
+            for(int i=0;i<numCourses;i++){
+              if(!fl.contains(i))
+                    fl.addFirst(i); 
+            }
+                
+            
+            //System.out.println(fl);            
+        
+       
+        for(int j:fl)
+            arr[idx++]=j;
+      return arr;  
+    }
+}
