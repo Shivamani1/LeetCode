9b039ae74c0abf9688b1c1884b53fc6a84b8130e
Shivamani1 <111039019+Shivamani1@users.noreply.github.com>
2024-04-07 13:50:57 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (29.73%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,23 +1,28 @@
 class Solution {
     public double myPow(double x, int n) {
         
-        if(n < 0){
-            n = -n;
+        // Base condition: If n is 0, x^0 is 1
+        if (n == 0) {
+            return 1;
+        }
+
+        // Convert n to a long integer to handle the edge case with Integer.MIN_VALUE
+        long N = n;
+
+        // If n is negative, take the reciprocal of x and make N positive
+        if (N < 0) {
+            N = -N;
             x = 1 / x;
         }
-        
-        double pow = 1;
-        
-        while(n != 0){
-            if((n & 1) != 0){
-                pow *= x;
-            } 
-                
-            x *= x;
-            n >>>= 1;
-            
+
+        // If N is even, recursively compute the square of x^(N/2)
+        if (N % 2 == 0) {
+            return myPow(x * x, (int) (N / 2));
+        } 
+    
+        // If N is odd, recursively compute x^(N-1) and multiply it by x
+        else {
+            return x * myPow(x, (int) (N - 1));
         }
-        
-        return pow;
     }
 }
