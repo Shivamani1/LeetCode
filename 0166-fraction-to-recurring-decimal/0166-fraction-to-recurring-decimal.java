b8253d99824e3774a93bd1b6848f5a761a38448a
Shivamani1 <111039019+Shivamani1@users.noreply.github.com>
2024-04-09 19:11:57 +0530

    Time: 2 ms (24.49%), Space: 40.5 MB (95.29%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
@@ -1,66 +1,39 @@
-class Solution {
+    public class Solution {
     public String fractionToDecimal(int numerator, int denominator) {
-        long Numerator=numerator;
-        long Denominator=denominator;
-StringBuilder sb=new StringBuilder();
-       long num=Math.abs(Numerator);
-       long den=Math.abs(Denominator);
-      
-     // System.out.println(Numerator*Denominator);
-         if(Numerator*Denominator<0){
-            sb.append("-"); 
-         }
-         long rem=Math.abs(num%den);    
-        
-        if(rem==0){
-        sb.append(num/den);
-            return sb.toString();
+        if (numerator == 0) {
+            return "0";
         }
-        sb.append(num/den);
-        sb.append(".");
-        List<List<Long>> l=new ArrayList();
-        StringBuilder o=new StringBuilder();
-        long q=0;
-        while(true){
-            rem=rem%den;
-              if(rem==0){   
-                for(int i=0;i<l.size();i++){
-                    o.append(l.get(i).get(0));
-                } 
-                  break;
-            }
-            
-            rem=rem*10;
-            q=rem/den;
-           // System.out.println(q);
-            
-            if(!l.contains(Arrays.asList(q,rem)))
-            l.add(Arrays.asList(q,rem));
-            else{
-               int x = l.indexOf(Arrays.asList(q,rem));  
-             
-                for(int i=0;i<x;i++)
-                    o.append(l.get(i).get(0));
-               
-                   o.append("(");
-                for(int i=x;i<l.size();i++){
-                    o.append(l.get(i).get(0));
-                }
-                 o.append(")");
+        StringBuilder res = new StringBuilder();
+        // "+" or "-"
+        res.append(((numerator > 0) ^ (denominator > 0)) ? "-" : "");
+        long num = Math.abs((long)numerator);
+        long den = Math.abs((long)denominator);
+        
+        // integral part
+        res.append(num / den);
+        num %= den;
+        if (num == 0) {
+            return res.toString();
+        }
+        
+        // fractional part
+        res.append(".");
+        HashMap<Long, Integer> map = new HashMap<Long, Integer>();
+        map.put(num, res.length());
+        while (num != 0) {
+            num *= 10;
+            res.append(num / den);
+            num %= den;
+            if (map.containsKey(num)) {
+                int index = map.get(num);
+                res.insert(index, "(");
+                res.append(")");
                 break;
-                 }
-            
-            
             }
-        
-        
-        
-        
-        sb.append(o);
-        
-       
-       
-        
-        return sb.toString();
+            else {
+                map.put(num, res.length());
+            }
+        }
+        return res.toString();
     }
 }
