72926eadd92110b55fbf5151898cc65c8f1b7d66
Shivamani1 <111039019+Shivamani1@users.noreply.github.com>
2024-08-19 23:01:48 +0530

    Time: 1 ms (100.00%), Space: 56.9 MB (78.66%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,26 +1,26 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int A[]=nums;
-         int n=A.length;
+        
+         int n=nums.length;
             for(int i=0;i<n;i++)
-                if(A[i]<1||A[i]>n+1)
-                    A[i]=n+2;
+                if(nums[i]<1||nums[i]>n+1)
+                    nums[i]=n+2;
         
         int temp=0;
             for(int i=0;i<n;i++){
-                temp=Math.abs(A[i]);
+                temp=Math.abs(nums[i]);
                 if(temp<n+1){
-                    if(A[temp-1]<0)
+                    if(nums[temp-1]<0)
                         continue;
                     else 
-                        A[temp-1]=-A[temp-1];
+                        nums[temp-1]=-nums[temp-1];
                 }
         }
        
              temp=0;
              int i;
             for(i=0;i<n;i++)
-                if(A[i]>0)
+                if(nums[i]>0)
                     return i+1;
                 
             return n+1;
