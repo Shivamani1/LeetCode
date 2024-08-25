1939ba7f989e31343158346dba4a2565c5091f44
Shivamani1 <111039019+Shivamani1@users.noreply.github.com>
2024-08-26 00:44:42 +0530

    Time: 1 ms (100.00%), Space: 46.3 MB (84.78%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.java
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.java
@@ -2,27 +2,19 @@ class Solution {
     public int[] rowAndMaximumOnes(int[][] mat) {
           int rows = mat.length;
         int cols = mat[0].length;
-
-        int i = 0; 
-        int j = cols - 1;
-            int prev=j;
-            int idx=0;
-        for(int arr[]:mat)
-            Arrays.sort(arr);
-        while (i < rows && j >= 0) {
-           if(mat[i][j]==1)
-               j--;
-            else
-                i++;
-            if(prev!=j){
-                idx=i;
-                prev=j;
+            int arr[]=new int[2];
+        for(int i=0;i<rows;i++){
+            int count=0;
+            for(int j=0;j<cols;j++){
+                count+=mat[i][j];
+            }
+            if(count>arr[1]){
+                arr[1]=count;
+                arr[0]=i;
             }
                 
+            
         }
-        int arr[]=new int[2];
-        arr[1]=cols-prev-1;
-        arr[0]=idx;
-        return arr; 
+        return arr;
     }
 }
