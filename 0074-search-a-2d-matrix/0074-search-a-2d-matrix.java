919c4c5186f6e8d3ec4fc0d112d72f843f1d8ff3
Shivamani1 <111039019+Shivamani1@users.noreply.github.com>
2024-08-25 22:10:09 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (34.63%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,18 +1,46 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m=matrix[0].length;
-        int n=matrix.length;
-        int l=0;
-        int h=n*m-1;
+        
+        int n=ColBS(matrix,target,0,matrix.length-1);
+        // System.out.println(n);
+        return RowBS(matrix,target,0,matrix[0].length-1,n);
+    
+        
+    }
+    static int ColBS(int arr[][],int target,int l,int h){
+        int ans=0;
         while(l<=h){
             int mid=(l+h)/2;
-            if(matrix[mid/m][mid%m]<target)
+            if(arr[mid][0]<target){
                 l=mid+1;
-            else if(matrix[mid/m][mid%m]>target)
+                ans=mid;
+            }
+            else if(arr[mid][0]>target)
                 h=mid-1;
             else
-                return true;   
+                return mid;
+        }
+        return ans;
+    }
+    static boolean RowBS(int arr[][],int target,int l,int h,int i){
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(arr[i][mid]<target)
+                l=mid+1;
+            else if(arr[i][mid]>target)
+                h=mid-1;
+            else
+                return true;
         }
         return false;
+        
+        
+        
     }
+    
+    
+    
+    
+    
+    
 }
