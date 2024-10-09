7e513d59eae8cb53c008898fcbe89a1e046a05ac
Shivamani1 <111039019+Shivamani1@users.noreply.github.com>
2024-10-09 18:39:28 +0530

    Time: 5 ms (22.34%), Space: 44.6 MB (7.64%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        LinkedHashMap<Integer,Integer> hm=new LinkedHashMap<>();
+        for(int i:nums1)
+            hm.put(i,-1);
+        Stack<Integer> s=new Stack<Integer>();
+        for(int i=nums2.length-1;i>=0;i--){
+           
+            while(!s.empty()&&s.peek()<nums2[i])
+                s.pop();
+            if(!s.empty()&&hm.containsKey(nums2[i])&&s.peek()>nums2[i])
+                hm.put(nums2[i],s.peek());
+            s.push(nums2[i]);
+            
+        }
+        for(int i=0;i<nums1.length;i++){
+            nums1[i]=hm.get(nums1[i]);
+        }
+            
+        return nums1;
+        
+    }
+}
