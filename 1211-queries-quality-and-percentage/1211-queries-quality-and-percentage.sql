56c682f8a29a7c8da349f0f5b6d3ae70da63529c
Shivamani1 <111039019+Shivamani1@users.noreply.github.com>
2024-11-19 01:05:21 +0530

    Time: 465 ms (22.93%), Space: 0B (100.00%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,12 @@
+
+# select query_name,round(avg(rating/position),2) as quality,
+# round(sum(case when rating < 3 then 1 else 0 end)*100/count(*),2)as poor_query_percentage from Queries
+# group by query_name
+select query_name,
+round(avg(cast(rating as decimal) / position), 2) as quality,
+round(sum(case when rating < 3 then 1 else 0 end) * 100 / count(*), 2) as poor_query_percentage
+from
+queries
+where query_name is not null
+group by
+query_name;
